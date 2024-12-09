c0c031a1126b885673806c3259c13e225eae9306
takahashim <takahashimm@gmail.com>
2024-12-09 16:36:15 +0900

    init

---
+++ lib/cosense.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cosense/version"
+
+module Cosense
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/cosense_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Cosense do
+  it "has a version number" do
+    expect(Cosense::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
