9774f03a2238c82b4297d218249e93c572fe615b
takahashim <takahashimm@gmail.com>
2025-01-08 14:07:59 +0900

    refactor

---
+++ lib/cosensee/api/page_data.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'open-uri'
+
+module Cosensee
+  module Api
+    # for page-data API
+    class PageData
+      def export(project_name:, sid:)
+        uri = "https://scrapbox.io/api/page-data/export/#{project_name}.json"
+        send_request(uri, sid)
+      end
+
+      def download(project_name:, sid:, filename:)
+        res = export(project_name:, sid:)
+
+        File.binwrite(filename, res)
+      end
+
+      private
+
+      def send_request(uri, sid)
+        cookies = "connect.sid=#{sid}"
+        parsed_uri = URI.parse(uri)
+        response = parsed_uri.open('Cookie' => cookies)
+        response.read
+      end
+    end
+  end
+end
