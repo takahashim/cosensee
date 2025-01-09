5c1726ab69b22a17af3930f67f1d3b55a65730a7
takahashim <takahashimm@gmail.com>
2025-01-09 12:50:11 +0900

    rubocop

--- lib/cosensee/tailwind_command.rb
+++ lib/cosensee/tailwind_command.rb
@@ -1,20 +1,22 @@
-require "tailwindcss/ruby"
+# frozen_string_literal: true
+
+require 'tailwindcss/ruby'
 
 module Cosensee
   # build TailwindCSS Command, based on https://github.com/rails/tailwindcss-rails
   module TailwindCommand
-    def self.compile_command(debug: false, **kwargs)
+    def self.compile_command(debug: false, **)
       command = [
-        Tailwindcss::Ruby.executable(**kwargs),
-        "-i", "assets/styles/input.css",
-        "-o", "public/styles/tailwind.css",
-        "-c", "tailwind.config.js"
+        Tailwindcss::Ruby.executable(**),
+        '-i', 'assets/styles/input.css',
+        '-o', 'public/styles/tailwind.css',
+        '-c', 'tailwind.config.js'
       ]
 
-      command << "--minify" unless debug
+      command << '--minify' unless debug
 
-      postcss_path = "postcss.config.js"
-      command += ["--postcss", postcss_path.to_s] if File.exist?(postcss_path)
+      postcss_path = 'postcss.config.js'
+      command += ['--postcss', postcss_path.to_s] if File.exist?(postcss_path)
 
       command
     end
