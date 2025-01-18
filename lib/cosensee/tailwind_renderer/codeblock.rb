4192f8296b2c57bef13757e58ddafd89cee0040f
takahashim <takahashimm@gmail.com>
2025-01-18 23:35:30 +0900

    feat: support inline SVG text extension

--- lib/cosensee/tailwind_renderer/codeblock.rb
+++ lib/cosensee/tailwind_renderer/codeblock.rb
@@ -2,13 +2,16 @@
 
 module Cosensee
   class TailwindRenderer
-    # connect parsed lines to a code block
-
+    # codeblock renderer
     Codeblock = Data.define(:content, :project) do
       include HtmlEncodable
 
       def render
         title = content.name
+
+        # !!! EXTENSION: if name ends with .svgtext, render as inline SVG text
+        return InlineSvgText.new(content:, project:).render if title.match?(/\.svgtext$/)
+
         <<~HTML_BLOCK
           <div class="bg-orange-300 text-gray-900 px-4 py-1 rounded-t-lg font-mono text-sm">#{title}</div>
           <div class="px-4 bg-gray-300 text-gray-900 rounded-b-lg shadow-md"><pre class="overflow-x-auto"><code class="block font-mono text-sm leading-relaxed">#{escape_html(content.content)}</code></pre></div>
---
+++ lib/cosensee/tailwind_renderer/inline_svg_text.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Cosensee
+  class TailwindRenderer
+    # extended renderer for inline SVG text
+    InlineSvgText = Data.define(:content, :project) do
+      include HtmlEncodable
+
+      def render
+        output = <<~SVG_TEXT_HEADER
+          <svg style="inherit" xmlns="http://www.w3.org/2000/svg" class="svg-text">
+            <style>
+              .text { -ms-user-select: none; -webkit-user-select: none; user-select: none; }
+              @media print { .svg-text { display: none !important; } text  { display: none !important; } }
+            </style>
+          <text x="0" y="0" font-size="inherit" class="text" fill="break">
+        SVG_TEXT_HEADER
+
+        content.content.lines.each do |line|
+          tspan_text = if line.strip.empty?
+                         '&#xA0;'
+                       else
+                         escape_html(line.chomp)
+                       end
+          output << %(<tspan x="0" dy="1.4em">#{tspan_text}</tspan>\n)
+        end
+
+        output << <<~SVG_TEXT_FOOTER
+            </text>
+          </svg>
+        SVG_TEXT_FOOTER
+
+        output
+      end
+    end
+  end
+end
