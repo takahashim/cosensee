d5b0324628ae4287889cdaa0f162ae4db9237cf1
takahashim <takahashimm@gmail.com>
2025-02-13 01:39:20 +0900

    refactor

--- lib/cosensee/tailwind_renderer/inline_svg_text.rb
+++ lib/cosensee/tailwind_renderer/inline_svg_text.rb
@@ -7,30 +7,31 @@ module Cosensee
       include HtmlEncodable
 
       def render
-        output = <<~SVG_TEXT_HEADER
+        svg_lines = content.content.lines.map do |line|
+          %(<tspan x="0" dy="1.4em">#{svg_line(line)}</tspan>\n)
+        end
+
+        <<~SVG_ELEMENT
           <svg style="inherit" xmlns="http://www.w3.org/2000/svg" class="svg-text">
             <style>
               .text { -ms-user-select: none; -webkit-user-select: none; user-select: none; }
               @media print { .svg-text { display: none !important; } text  { display: none !important; } }
             </style>
-          <text x="0" y="0" font-size="inherit" class="text" fill="break">
-        SVG_TEXT_HEADER
-
-        content.content.lines.each do |line|
-          tspan_text = if line.strip.empty?
-                         '&#xA0;'
-                       else
-                         escape_html(line.chomp)
-                       end
-          output << %(<tspan x="0" dy="1.4em">#{tspan_text}</tspan>\n)
-        end
-
-        output << <<~SVG_TEXT_FOOTER
+            <text x="0" y="0" font-size="inherit" class="text" fill="break">
+            #{svg_lines.join}
             </text>
           </svg>
-        SVG_TEXT_FOOTER
+        SVG_ELEMENT
+      end
 
-        output
+      private
+
+      def svg_line(line)
+        if line.strip.empty?
+          '&#xA0;'
+        else
+          escape_html(line.chomp)
+        end
       end
     end
   end
