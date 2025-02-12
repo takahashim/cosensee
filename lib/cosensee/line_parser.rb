027ab69ce901dcd00cbfbcd90803056cd4bd6518
takahashim <takahashimm@gmail.com>
2025-02-13 01:37:21 +0900

    rubocop

--- lib/cosensee/line_parser.rb
+++ lib/cosensee/line_parser.rb
@@ -28,7 +28,7 @@ module Cosensee
     end
 
     def initialize
-      @bracket_parser = Cosensee::BracketParser.new
+      @bracket_parser = BracketParser.new
     end
 
     def parse(line)
@@ -46,35 +46,42 @@ module Cosensee
 
     def parse_indent(line)
       matched = line.match(INDENT_PATTERN)
-      ParsedLine.new(indent: Cosensee::Node::Indent.new(matched[1], matched[1]),
-                     rest: matched[2], raw: line.raw)
+      ParsedLine.new(
+        indent: Node::Indent.new(matched[1], matched[1]),
+        rest: matched[2],
+        raw: line.raw
+      )
     end
 
     def parse_whole_line(line)
       # parse quote
       matched = line.match(QUOTE_PATTERN)
       if matched
-        line.update(rest: matched[2],
-                    line_content: Cosensee::Node::Quote.new(content: nil, raw: matched[0], mark: matched[1]))
+        line.update(
+          rest: matched[2],
+          line_content: Node::Quote.new(content: nil, raw: matched[0], mark: matched[1])
+        )
         return line
       end
 
       # parse codeblock
       matched = line.match(CODEBLOCK_PATTERN)
       if matched
-        return line.update(rest: nil,
-                           line_content: Cosensee::Node::Codeblock.new(content: '', name: matched[2], raw: matched[0]),
-                           parsed: true)
+        return line.update(
+          rest: nil,
+          line_content: Node::Codeblock.new(content: '', name: matched[2], raw: matched[0]),
+          parsed: true
+        )
       end
 
       # parse command line
       matched = line.match(COMMANDLINE_PATTERN)
       if matched
-        return line.update(rest: nil,
-                           line_content: Cosensee::Node::CommandLine.new(content: matched[2],
-                                                                         prompt: matched[1],
-                                                                         raw: matched[0]),
-                           parsed: true)
+        return line.update(
+          rest: nil,
+          line_content: Node::CommandLine.new(content: matched[2], prompt: matched[1], raw: matched[0]),
+          parsed: true
+        )
       end
 
       line
@@ -87,24 +94,29 @@ module Cosensee
       strs = line.split_rest_by('`')
       loop do
         str = strs.shift
-        unless str
-          return line.update(rest: nil,
-                             content: parsed)
+        if str.nil?
+          return line.update(
+            rest: nil,
+            content: parsed
+          )
         end
 
         parsed << str
 
         str = strs.shift
-        unless str
-          line.update(rest: nil,
-                      content: parsed)
-          return line
+        if str.nil?
+          return line.update(
+            rest: nil,
+            content: parsed
+          )
         end
 
         if strs.empty?
           parsed.last.concat("`#{str}")
-          return line.update(rest: nil,
-                             content: parsed)
+          return line.update(
+            rest: nil,
+            content: parsed
+          )
         else
           parsed << Node::Code.new(str, "`#{str}`")
         end
@@ -122,11 +134,11 @@ module Cosensee
             matched = elem.match(/(^|\s)#(\S+)/)
             if matched
               parsed << "#{matched.pre_match}#{matched[1]}"
-              parsed << Cosensee::Node::HashTag.new(content: matched[2], raw: "##{matched[2]}")
+              parsed << Node::HashTag.new(content: matched[2], raw: "##{matched[2]}")
               elem = matched.post_match
             else
               parsed << elem
-              break # loop
+              break # from loop
             end
           end
         else
@@ -148,7 +160,7 @@ module Cosensee
             matched = elem.match(%r{(^|.*?)(https?://[^\s]+)})
             if matched
               parsed << "#{matched.pre_match}#{matched[1]}"
-              parsed << Cosensee::Node::Link.new(matched[2], matched[2])
+              parsed << Node::Link.new(matched[2], matched[2])
               elem = matched.post_match
             else
               parsed << elem
@@ -174,7 +186,7 @@ module Cosensee
             matched = elem.match(/\[\[(.+?)\]\]/)
             if matched
               parsed << matched.pre_match
-              parsed << Cosensee::Node::DoubleBracket.new(content: [matched[1]], raw: matched[0])
+              parsed << Node::DoubleBracket.new(content: [matched[1]], raw: matched[0])
               elem = matched.post_match
             else
               parsed << elem
@@ -198,7 +210,7 @@ module Cosensee
 
       line.content.each do |elem|
         case elem
-        when Cosensee::Node::Code, Cosensee::Node::DoubleBracket
+        when Node::Code, Node::DoubleBracket
           if target_char == '['
             parsed << elem
           else
@@ -242,7 +254,7 @@ module Cosensee
 
     def done_parsing(line)
       # If the line_content is Cosensee::Node::Quote, move content into it.
-      if line.line_content.is_a?(Cosensee::Node::Quote)
+      if line.line_content.is_a?(Node::Quote)
         new_quote = line.line_content.replace_content(line.content)
         line.update(line_content: new_quote, content: [])
       end
